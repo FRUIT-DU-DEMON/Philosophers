50e30c5456c26bff0285c5112215ce2edce0bdba
Halim Labouiti <hlabouit@e3r5p1.1337.ma>
2023-07-11 02:22:31 +0100

    done......i guess! who can trust threads

--- philo/philo.h
+++ philo/philo.h
@@ -6,64 +6,49 @@
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 20:46:13 by hlabouit          #+#    #+#             */
-/*   Updated: 2023/07/09 17:50:37 by hlabouit         ###   ########.fr       */
+/*   Updated: 2023/07/11 01:40:46 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
-#define PHILO_H
+# define PHILO_H
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<pthread.h>
-#include<sys/time.h>
+# include<stdio.h>
+# include<stdlib.h>
+# include<unistd.h>
+# include<pthread.h>
+# include<sys/time.h>
 
 typedef struct t_philo_data
 {
-    pthread_t philo;
-    pthread_mutex_t fork;
-    pthread_mutex_t next_fork;
-    pthread_mutex_t routine_dispaly;
-    pthread_mutex_t last_meal_time_update;
-    int ph_id;
-    int ph_nb;
-    int t_to_die;
-    int t_to_eat;
-    int t_to_sleep;
-    int total_meals;
-    int meals_eaten;
-    long long last_meal_time;
-    long long start_time;
-} t_philo_data;
+	pthread_t		philo;
+	pthread_mutex_t	fork;
+	pthread_mutex_t	*next_fork;
+	pthread_mutex_t	*routine_dispaly;
+	pthread_mutex_t	last_meal_time_update;
+	pthread_mutex_t	meals_eaten_update;
+	int				ph_id;
+	int				ph_nb;
+	int				t_to_die;
+	int				t_to_eat;
+	int				t_to_sleep;
+	int				total_meals;
+	int				meals_eaten;
+	long long		last_meal_time;
+	long long		start_time;
+}	t_philo_data;
 
-
-int     len_of_input(char **av);
-char    *join_input(char **av);
-char    **split_input(char **av);
-char	**ft_split(char *s, char c);
-char	*ft_strncpy(char *s1, char *s2, int n);
-int     ft_isdigit(int c);
-int	    ft_strlen(char *s);
-int     ft_atoi(char *str);
-int     long_negative_nb_error(char **av);
-int     not_integer_error(char **av);
-int     check_input_errors(char **av);
-
-
-
-
-
-long long get_current_time(void);
-
-
-
-
- 
-
-
-
-
-void	free_memory(char **output);
-
+int			len_of_input(char **av);
+char		*join_input(char **av);
+char		**split_input(char **av);
+char		**ft_split(char *s, char c);
+char		*ft_strncpy(char *s1, char *s2, int n);
+int			ft_isdigit(int c);
+int			ft_strlen(char *s);
+int			ft_atoi(char *str);
+int			long_negative_nb_error(char **av);
+int			not_integer_error(char **av);
+int			check_input_errors(char **av);
+long long	get_current_time(void);
+void		free_memory(char **output);
 #endif
--- philo/routine.c
+++ philo/routine.c
@@ -6,26 +6,24 @@
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/02 01:37:59 by hlabouit          #+#    #+#             */
-/*   Updated: 2023/07/10 01:15:53 by hlabouit         ###   ########.fr       */
+/*   Updated: 2023/07/11 01:46:54 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"philo.h"
 
-t_philo_data *initialize_data(char **av)
+t_philo_data	*initialize_data(char **av)
 {
-	int i;
-	int id;
-	int ph_nb;
-	t_philo_data *philo;
+	t_philo_data	*philo;
+	int				i;
+	int				ph_nb;
 
 	i = 0;
-	id = 1;
 	ph_nb = ft_atoi(av[1]);
 	philo = malloc(ph_nb * sizeof(t_philo_data));
-	while(i < ph_nb)
+	while (i < ph_nb)
 	{
-		philo[i].ph_id = id;
+		philo[i].ph_id = i + 1;
 		philo[i].ph_nb = ph_nb;
 		philo[i].t_to_die = ft_atoi(av[2]);
 		philo[i].t_to_eat = ft_atoi(av[3]);
@@ -38,76 +36,73 @@ t_philo_data *initialize_data(char **av)
 		philo[i].last_meal_time = get_current_time();
 		philo[i].start_time = get_current_time();
 		i++;
-		id++;
 	}
-	return(philo);
+	return (philo);
 }
 
-long long get_current_time(void)
+long long	get_current_time(void)
 {
-	struct timeval tv;
-	long long current_time;
+	struct timeval	tv;
+	long long		current_time;
 	
 	gettimeofday(&tv, NULL);
 	current_time = (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
 	return(current_time);
 }
 
-void customized_usleep(int sleep_duration)
+void	customized_usleep(int sleep_duration)
 {
-	long long current_time;
+	long long	current_time;
 
 	current_time = get_current_time();
 	while (get_current_time() - current_time < sleep_duration)
-    	usleep(sleep_duration);
+		usleep(sleep_duration);
 }
 
 void	*routine(void *ptr)
 {
-	t_philo_data *philo;
+	t_philo_data	*philo;
 
 	philo = (t_philo_data *)ptr;
 	if (philo->ph_id % 2 != 0)
-		usleep(5);
-	while(1)
+		usleep(100);
+	while (1)
 	{
+		
 		pthread_mutex_lock(&philo->fork);
+		pthread_mutex_lock(philo->routine_dispaly);
+		printf("%lld %d has taken a fork\n", (get_current_time() - philo->start_time), philo->ph_id);
+		pthread_mutex_unlock(philo->routine_dispaly);
 		
-		pthread_mutex_lock(&philo->routine_dispaly);
-		printf("\n timestamp = %lld id = %d has taken a fork\n", (get_current_time() - philo->start_time), philo->ph_id);
-		pthread_mutex_unlock(&philo->routine_dispaly);
+		pthread_mutex_lock(philo->next_fork);
+		pthread_mutex_lock(philo->routine_dispaly);
+		printf("%lld %d has taken a fork\n", (get_current_time() - philo->start_time), philo->ph_id);
+		pthread_mutex_unlock(philo->routine_dispaly);
 		
-		pthread_mutex_lock(&philo->next_fork);
-		
-		pthread_mutex_lock(&philo->routine_dispaly);
-		printf("\n timestamp = %lld id = %d has taken a fork 2\n", (get_current_time() - philo->start_time), philo->ph_id);
-		pthread_mutex_unlock(&philo->routine_dispaly);
-		
-		pthread_mutex_lock(&philo->routine_dispaly);
-		printf("\n timestamp = %lld id = %d eating\n", (get_current_time() - philo->start_time), philo->ph_id);
-		pthread_mutex_unlock(&philo->routine_dispaly);
 		customized_usleep(philo->t_to_eat);
-		
-		
 		pthread_mutex_lock(&philo->last_meal_time_update);
-		philo->meals_eaten++;
 		philo->last_meal_time = get_current_time();
 		pthread_mutex_unlock(&philo->last_meal_time_update);
 		
+		pthread_mutex_lock(&philo->meals_eaten_update);
+		philo->meals_eaten++;
+		pthread_mutex_unlock(&philo->meals_eaten_update);
+		
+		pthread_mutex_unlock(philo->next_fork);
+		pthread_mutex_unlock(&philo->fork);
+		
+		pthread_mutex_lock(philo->routine_dispaly);
+		printf("%lld %d is eating\n", (get_current_time() - philo->start_time), philo->ph_id);
+		pthread_mutex_unlock(philo->routine_dispaly);
+		
+		pthread_mutex_lock(philo->routine_dispaly);
+		printf("%lld %d is sleeping\n", (get_current_time() - philo->start_time), philo->ph_id);
+		pthread_mutex_unlock(philo->routine_dispaly);
 		customized_usleep(philo->t_to_sleep);
 		
-		pthread_mutex_lock(&philo->routine_dispaly);
-		printf("\n timestamp = %lld id = %d sleeping\n", (get_current_time() - philo->start_time), philo->ph_id);
-		pthread_mutex_unlock(&philo->routine_dispaly);
-		
-		
-		pthread_mutex_lock(&philo->routine_dispaly);
-		printf("\n timestamp = %lld id = %d thinking\n", (get_current_time() - philo->start_time), philo->ph_id);
-		pthread_mutex_unlock(&philo->routine_dispaly);
-		
-		pthread_mutex_unlock(&philo->fork);
-		pthread_mutex_unlock(&philo->next_fork);
-		
+		pthread_mutex_lock(philo->routine_dispaly);
+		printf("%lld %d is thinking\n", (get_current_time() - philo->start_time), philo->ph_id);
+		pthread_mutex_unlock(philo->routine_dispaly);
 	}
 	return (ptr);
 }
@@ -116,41 +111,37 @@ int	threading_philos(t_philo_data *table)
 {
 	int i;
 
+	pthread_mutex_t *routine_dispaly = malloc(sizeof(pthread_mutex_t));
+	pthread_mutex_init(routine_dispaly, NULL);
+	
 	i = 0;
-	pthread_mutex_init(&table->routine_dispaly, NULL);
-	pthread_mutex_init(&table->last_meal_time_update, NULL);
 	while(i < table->ph_nb)
 	{
+		table[i].routine_dispaly = routine_dispaly;
+		pthread_mutex_init(&table[i].last_meal_time_update, NULL);
+		pthread_mutex_init(&table[i].meals_eaten_update, NULL);
 		pthread_mutex_init(&table[i].fork, NULL);
 		i++;
 	}
 	i = 0;
-	while(i < table->ph_nb)
+	while (i < table->ph_nb)
 	{
 		if (table[i].ph_id == table->ph_nb)
 		{
-			table[i].next_fork = table[0].fork;
+			table[i].next_fork = &table[0].fork;
 			break;
 		}
-		table[i].next_fork = table[i + 1].fork;
+		table[i].next_fork = &table[i + 1].fork;
 		i++;
 	}
 	i = 0;
-	while(i < table->ph_nb)
+	while (i < table->ph_nb)
 	{
 		if (pthread_create(&table[i].philo, NULL, &routine, &table[i]) != 0)
 		{
 			printf("Error\npthread_create function has failed\n");
 			return (1);
 		}
-		i++;
-	}
-	i = 0;
-	pthread_mutex_destroy(&table->routine_dispaly);
-	pthread_mutex_destroy(&table->last_meal_time_update);
-	while(i < table->ph_nb)
-	{
-		pthread_mutex_destroy(&table[i].fork);
 		if (pthread_detach(table[i].philo) != 0)
 		{
 			printf("Error\npthread_detach function has failed\n");
@@ -163,8 +154,8 @@ int	threading_philos(t_philo_data *table)
 
 int	check_philo_state(t_philo_data *table)
 {
-	int i;
-	int flag;
+	int	i;
+	int	flag;
 
 	flag = 0;
 	while (1)
@@ -172,27 +163,26 @@ int	check_philo_state(t_philo_data *table)
 		i = 0;
 		while (i < table->ph_nb)
 		{
-			usleep(10);
-			pthread_mutex_lock(&table->last_meal_time_update);
+			usleep(100);
+			pthread_mutex_lock(&table[i].last_meal_time_update);
 			if (get_current_time() - table[i].last_meal_time >= table->t_to_die)
 			{
-				pthread_mutex_lock(&table->routine_dispaly);
-				printf("\n timestamp = %lld id = %d died!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n", (get_current_time() - table[i].start_time), table[i].ph_id);
+				pthread_mutex_lock(table[i].routine_dispaly);
+				printf("%lld %d died\n", (get_current_time() - table[i].start_time), table[i].ph_id);
 				return (0);
 			}
-			// pthread_mutex_unlock(&table->last_meal_time_update);
-			// pthread_mutex_lock(&table->last_meal_time_update);
+			pthread_mutex_unlock(&table[i].last_meal_time_update);
+			pthread_mutex_lock(&table[i].meals_eaten_update);
 			if (table[i].meals_eaten == table->total_meals)
 			{
 				flag++;
 				if (flag == table->ph_nb)
 				{
-					
 					printf("\nall philos got at least %d meals\n", table->total_meals);
 					return(table->total_meals);
 				}
 			}
-			pthread_mutex_unlock(&table->last_meal_time_update);
+			pthread_mutex_unlock(&table[i].meals_eaten_update);
 			i++;
 		}
 	}
@@ -208,11 +198,22 @@ int main(int ac, char **av)
 	}
 	if (check_input_errors(av) == 1)
 		return (1);
-	t_philo_data *table;
+	t_philo_data	*table;
 
 	table = initialize_data(av);
 	threading_philos(table);
-	if (check_philo_state(table) == 0 || check_philo_state(table) == table->total_meals)
+	int state = check_philo_state(table);
+	if (state == 0 || state == table->total_meals)
+	{
+		int i = 0;
+		while(i < table->ph_nb)
+		{
+		// pthread_mutex_destroy(&table[i].routine_dispaly);
+		// pthread_mutex_destroy(&table[i].last_meal_time_update);
+		// pthread_mutex_destroy(&table[i].fork);
+		i++;
+		}	
 		return (0);
+	}
 	
 }
